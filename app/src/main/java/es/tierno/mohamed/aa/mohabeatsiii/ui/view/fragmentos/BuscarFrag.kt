f6a3f96efca79625fe7f0e95791e9e0bce13dab3
Mohamed Afallah <mohaparla184@gmail.com>
2025-04-28 09:13:08 +0200

    "He añadido los fragmentos para explorar u el fragmento para buscar"

---
+++ app/src/main/java/es/tierno/mohamed/aa/mohabeatsiii/ui/view/fragmentos/BuscarFrag.kt
@@ -0,0 +1,12 @@
+package es.tierno.mohamed.aa.mohabeatsiii.ui.view.fragmentos
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import es.tierno.mohamed.aa.mohabeatsiii.R
+
+class BuscarFrag : Fragment() {
+
+}
